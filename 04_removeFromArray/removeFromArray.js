aaef5b0d465ab6da8bea2f12bececcc1c1b41354
OatAmOats <vicente.i.tapia@gmail.com>
2023-07-26 18:00:43 -0400

    oopsies

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const myArray = [];
 
+    arr.forEach((item) =>{
+        if(!args.includes(item)){
+            myArray.push(item);
+        }
+
+    });
+    return(myArray);
 };
 
 // Do not edit below this line
